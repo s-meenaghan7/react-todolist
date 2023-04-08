d0453ea47e9d0f13a0101ec207af8c90d7ce8b47
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-04-07 18:26:56 -0700

    Implemented updating Todos. Clicking the edit button allows the todo data to be changed. Clicking enter inside the input, or simply clicking anywhere outside the input, will save the updated todo data

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Todo.css';
 
 export default function Todo({ todo, removeTodo }) {
+  const [thisTodo, setThisTodo] = useState(todo.todo);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const updateThisTodo = (e) => {
+    e.preventDefault();
+
+    if (thisTodo.trim() !== '')
+      setIsEditing(false);
+  }
+
   return (
     <div className='todo'>
-      {todo.todo}
+      {
+        !isEditing ?
+          thisTodo
+          :
+          <form onSubmit={updateThisTodo}>
+            <input
+              type='text'
+              value={thisTodo}
+              onChange={(e) => setThisTodo(e.target.value)}
+              onBlur={(e) => updateThisTodo(e)}
+            />
+            <input type='submit' style={{ "display": "none" }} />
+          </form>
+      }
       <div>
         <button
           type='button'
-          className='delete-btn'
+          id='edit-btn'
+          onClick={() => setIsEditing(true)}
+        >
+          EDIT
+        </button>
+        <button
+          type='button'
+          id='delete-btn'
           onClick={() => removeTodo(todo.id)}
         >
           DELETE
