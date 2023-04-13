be7ebe942a62a1ee65cf2c03dd7008e83e4b9430
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-04-13 04:35:15 -0700

    Made MenuBar title 'My Todos' smaller by changing it to an h2 and container element changed to header for accessibility

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -3,15 +3,15 @@ import '../styles/MenuBar.css';
 
 export default function MenuBar({ todoCount }) {
   return (
-    <div className='menu-bar'>
+    <header className='menu-bar'>
       <div style={{ "marginLeft": "25px"}}>
-        <h1>My Todos</h1>
+        <h2>My Todos</h2>
       </div>
       <div>
         <span style={{ "marginRight": "25px" }}>
           {todoCount} {todoCount === 1 ? 'Todo' : 'Todos'}
         </span>
       </div>
-    </div>
+    </header>
   );
 }
