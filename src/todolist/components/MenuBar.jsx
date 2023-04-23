7c74c66f6933f08bbf2a4333b53c3ea171ec4253
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-04-23 16:28:48 -0700

    Decided to remove menu bar button. Became a lot more complicated, spent a lot of time with it and decided it was not currently worth the time and effort to implement the right way.

--- src/todolist/components/MenuBar.jsx
+++ src/todolist/components/MenuBar.jsx
@@ -4,21 +4,7 @@ import '../styles/MenuBar.css';
 export default function MenuBar({ todoCount, showCompletedTodos }) {
   return (
     <header className='menu-bar'>
-      <div className='header'>
-        <button
-          id='menu-btn'
-          type='button'
-          disabled={showCompletedTodos}
-        >
-          <span
-            className="material-icons-outlined"
-            style={{ "fontSize": "25px" }}
-          >
-            menu
-          </span>
-        </button>
-        <h2>My Todos</h2>
-      </div>
+      <h2>My Todos</h2>
       <div>
         <span style={{ "marginRight": "25px" }}>
           {todoCount} {todoCount === 1 ? 'Todo' : 'Todos'} {showCompletedTodos ? "Complete" : "In Progress"}
